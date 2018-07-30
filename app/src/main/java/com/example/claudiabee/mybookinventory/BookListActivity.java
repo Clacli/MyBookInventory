a7d09a5c1b8a9953332ef13176d6ed23f6d535f6
ClaudiaBee <claudiabellan.herb@gmail.com>
2018-07-30 22:19:37 +0200

    Add menu options to the BookListActivity AppBar, when chosen display toast message for now

--- app/src/main/java/com/example/claudiabee/mybookinventory/BookListActivity.java
+++ app/src/main/java/com/example/claudiabee/mybookinventory/BookListActivity.java
@@ -3,6 +3,8 @@ package com.example.claudiabee.mybookinventory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -24,5 +26,31 @@ public class BookListActivity extends AppCompatActivity {
                 Toast.makeText(BookListActivity.this, "This will open another activity soon", Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Add menu option buttons to the app bar
+        getMenuInflater().inflate(R.menu.book_list_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Start action related to the menu option clicked by user from overflow menu in the app bar
+        switch (item.getItemId()) {
+            // Insert sample book data if user click this option menu
+            case R.id.action_add_sample_data:
+                // Insert sample data in database
+                // Update the text displayed on the screen in the sample TextView
+                Toast.makeText(BookListActivity.this, "Clicking this will add a sample record about a book", Toast.LENGTH_LONG).show();
+                return true;
+            // Delete all record about book in the table
+            case R.id.action_delete_all_data:
+                // Implement later
+                Toast.makeText(BookListActivity.this, "This will delete all of the database books table", Toast.LENGTH_LONG).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
