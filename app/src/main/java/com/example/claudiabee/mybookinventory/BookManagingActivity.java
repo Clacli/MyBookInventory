f0252cf332632064ac03426fc1960a618f7777ba
ClaudiaBee <claudiabellan.herb@gmail.com>
2018-07-31 05:32:14 +0200

    Add Fab to BookManagingActivity

--- app/src/main/java/com/example/claudiabee/mybookinventory/BookManagingActivity.java
+++ app/src/main/java/com/example/claudiabee/mybookinventory/BookManagingActivity.java
@@ -1,7 +1,10 @@
 package com.example.claudiabee.mybookinventory;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class BookManagingActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class BookManagingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_managing);
+
+        // Find the Fab
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab2);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(BookManagingActivity.this, "This will add the book to the db and take back to BookListActivity", Toast.LENGTH_LONG).show();
+                // InsertData();
+                finish();
+            }
+        });
+
     }
 }
