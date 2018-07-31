d2191bb010f3028d465dce52d8a03a65a0a1b161
ClaudiaBee <claudiabellan.herb@gmail.com>
2018-07-31 03:11:12 +0200

    Rename Activity and provide layout

--- app/src/main/java/com/example/claudiabee/mybookinventory/BookManagingActivity.java
+++ app/src/main/java/com/example/claudiabee/mybookinventory/BookManagingActivity.java
@@ -3,11 +3,11 @@ package com.example.claudiabee.mybookinventory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ManagingActivity extends AppCompatActivity {
+public class BookManagingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_managing);
+        setContentView(R.layout.activity_book_managing);
     }
 }
