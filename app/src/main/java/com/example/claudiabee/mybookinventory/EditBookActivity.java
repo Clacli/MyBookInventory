427e3e09e8f88c59357aa9b07ddcbd6d894a8b45
ClaudiaBee <claudiabellan.herb@gmail.com>
2018-08-11 03:11:30 +0200

    Add EditBookActivity and its layout

---
+++ app/src/main/java/com/example/claudiabee/mybookinventory/EditBookActivity.java
@@ -0,0 +1,13 @@
+package com.example.claudiabee.mybookinventory;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EditBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_book);
+    }
+}
